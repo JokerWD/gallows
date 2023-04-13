7e9d9b82e7f33deaf64af30cd451d0b0be4d8680
JokerWD <80598226+JokerWD@users.noreply.github.com>
2023-04-14 00:04:35 +0300

    gallows 1.5

--- src/gallows.java
+++ src/gallows.java
@@ -1,45 +1,66 @@
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class gallows {
     public static void main(String[] args) {
-        String word;
-        int attempts = 5;
         Scanner scanner = new Scanner(System.in);
-        word = scanner.nextLine();
-
-        char[] guessesWord = new char[word.length()];
-        for (int i = 0; i < guessesWord.length; i++) {
-            guessesWord[i] = '_';
-        }
+        String word = scanner.nextLine();
+        int attempts = 5;
+        StringBuilder guessesWord = initializeGuessesWord(word);
+        Set<Character> enteredLetters = new HashSet<>();
 
         while (attempts > 0 && !isComplete(guessesWord)){
-            System.out.println("Текущая состояния альфавита " + String.valueOf(guessesWord));
-            System.out.println("Колличество попыток " + attempts);
-            System.out.print("Введите букву: ");
-            char letter = scanner.next().toLowerCase().charAt(0);
-
-            boolean found = false;
-            for (int i = 0; i < word.length(); i++) {
-                if(word.charAt(i) == letter && guessesWord[i] == '_'){
-                    guessesWord[i] = letter;
-                    found = true;
-                }
+            printCurrentState(guessesWord, attempts);
+            char letter = getUserInput(scanner);
+            if(enteredLetters.contains(letter)){
+                System.out.println("----------------------------------------");
+                System.out.println("Ты уже вводил эту букву, попробуй другую");
+                System.out.println("----------------------------------------");
+                continue;
             }
+
+            enteredLetters.add(letter);
+
+            boolean found = checkAndUpdateLatter(word, guessesWord, letter );
             if(!found){
                 attempts--;
             }
         }
-
-        scanner.close();
+        printResult(isComplete(guessesWord), word);
     }
-
-    private static boolean isComplete(char[] guessesWord) {
-        for (char c : guessesWord) {
-            if (c == '_') {
-                return false;
+    private static StringBuilder initializeGuessesWord(String word){
+        StringBuilder guessesWord = new StringBuilder(word.length());
+        word.chars().forEach(c -> guessesWord.append('_'));
+        return guessesWord;
+    }
+    private static void printCurrentState(StringBuilder guessesWord, int attempts){
+        System.out.println("Текущее состояние слова: " + guessesWord);
+        System.out.println("Колличество попыток: " + attempts);
+    }
+    private static char getUserInput(Scanner scanner){
+        System.out.print("Введите букву: ");
+        return scanner.next().toLowerCase().charAt(0);
+    }
+    private static boolean checkAndUpdateLatter(String word, StringBuilder guessesWord, char letter){
+        boolean found = false;
+        for (int i = 0; i < word.length(); i++) {
+            if(word.charAt(i) == letter && guessesWord.charAt(i) == '_'){
+                guessesWord.setCharAt(i, letter);
+                found = true;
             }
         }
-        return true;
+        return found;
     }
+    private static boolean isComplete(StringBuilder guessesWord){
+        return !guessesWord.toString().contains("_");
+    }
+    private static void printResult(boolean isComplete, String word){
+        if(isComplete){
+            System.out.println("Ты победил! Слово: " + word);
+        }else {
+            System.out.println("Ты проиграл! Слово: " + word);
+        }
+    }
+
 }
