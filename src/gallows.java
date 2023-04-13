4385fc8e2aa526f2ed25a6035e5e41bea84c41d6
JokerWD <joker.osnova1902@gmail.com>
2023-04-13 18:23:33 +0300

    Version 1.0

--- src/gallows.java
+++ src/gallows.java
@@ -5,41 +5,41 @@ public class gallows {
     public static void main(String[] args) {
         String word;
         int attempts = 5;
-        try (Scanner scanner = new Scanner(System.in)) {
-            word = scanner.nextLine();
-        }
+        Scanner scanner = new Scanner(System.in);
+        word = scanner.nextLine();
+
         char[] guessesWord = new char[word.length()];
         for (int i = 0; i < guessesWord.length; i++) {
             guessesWord[i] = '_';
         }
 
-        try (Scanner scanner = new Scanner(System.in)) {
-            while (attempts > 0 && isComplete(guessesWord)){
-                System.out.println("Текущая состояния альфавита " + new String(guessesWord));
-                System.out.println("Колличество попыток " + attempts);
-                System.out.println("Введите букву: ");
-                char letter = scanner.next().toLowerCase().charAt(0);
+        while (attempts > 0 && !isComplete(guessesWord)){
+            System.out.println("Текущая состояния альфавита " + String.valueOf(guessesWord));
+            System.out.println("Колличество попыток " + attempts);
+            System.out.print("Введите букву: ");
+            char letter = scanner.next().toLowerCase().charAt(0);
 
-                boolean found = false;
-                for (int i = 0; i < word.length(); i++) {
-                    if(word.charAt(i) == letter && guessesWord[i] == '_'){
-                        guessesWord[i] = letter;
-                        found = true;
-                    }
+            boolean found = false;
+            for (int i = 0; i < word.length(); i++) {
+                if(word.charAt(i) == letter && guessesWord[i] == '_'){
+                    guessesWord[i] = letter;
+                    found = true;
                 }
-                if(!found){
-                    attempts --;
-                }
+            }
+            if(!found){
+                attempts--;
             }
         }
 
+        scanner.close();
     }
-    private static boolean isComplete(char[] guessedWord){
-        for (char value: guessedWord){
-            if(value == '_'){
-                return true;
+
+    private static boolean isComplete(char[] guessesWord) {
+        for (char c : guessesWord) {
+            if (c == '_') {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 }
